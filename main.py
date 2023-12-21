292c4657450526c8dcd3b6936fbe84c64a7b26a3
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-12-21 05:20:54 -0800

    Add keyboard markup and callback query handling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 import os
 from dotenv import load_dotenv
+from telebot import types
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -13,14 +14,29 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.reply_to(message, 'Hello! I am your Telegram bot.')
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton('Play')
+    itembtn2 = types.KeyboardButton('Help')
+    markup.add(itembtn1, itembtn2)
+    bot.send_message(message.chat.id, "Welcome to Nodemon", reply_markup=markup)
 
+    # keyboard = types.InlineKeyboardMarkup(row_width=2)
+    # play_button = types.InlineKeyboardButton(text='Play', callback_data='play')
+    # help_button = types.InlineKeyboardButton(text='Help', callback_data='help')
+    # keyboard.add(play_button, help_button)
+    
+    bot.send_message(message.chat.id, 'Welcome to Nodemon', reply_markup=markup)
 # Handle all other messages
-
+@bot.callback_query_handler(func=lambda call: True)
+def handle_query(call):
+    if call.data == 'play':
+        bot.send_message(call.message.chat.id, 'play with friends')
+    elif call.data == 'help':
+        bot.send_message(call.message.chat.id, 'help')
 
 @bot.message_handler(commands=['help'])
 def handle_help(message):
-    bot.reply_to(message, 'This is a help message.')
+    bot.reply_to(message, 'This is a nodemon .')
 
 
 @bot.message_handler(commands=['stop'])
@@ -32,8 +48,13 @@ def handle_ranking(message):
 def handle_message(message):
     bot.reply_to(message, 'You said: ' + message.text)
 
-
-print("Bot is running...")
+# @bot.callback_query_handler(func=lambda call: True)
+# def handle_button_click(call):
+#     if call.data == 'play':
+#         bot.send_message(call.message.chat.id, 'play with friends')
+#     elif call.data == 'help':
+#         bot.send_message(call.message.chat.id, 'help')
+# print("Bot is running...")
 
 # Start the bot
 bot.polling()
