dd24f99708398c420f276a96a835a4bef3ad2b0e
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2023-12-21 00:17:40 -0800

    Add new commands and environment variable support

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import telebot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
 # Create a new bot instance
-bot = telebot.TeleBot('6860778430:AAGFxWzKy5nPhmarJwLNu9KM-BWsl4Z79OU')
+bot = telebot.TeleBot(TOKEN)
 
 # Handle the /start command
+
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     bot.reply_to(message, 'Hello! I am your Telegram bot.')
 
 # Handle all other messages
+
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.reply_to(message, 'This is a help message.')
+
+
+@bot.message_handler(commands=['stop'])
+def handle_ranking(message):
+    bot.reply_to(message, 'This is the stop message.')
+
+
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     bot.reply_to(message, 'You said: ' + message.text)
+
+
+print("Bot is running...")
 
 # Start the bot
 bot.polling()
